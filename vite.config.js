91cb770ecfbe13915057bedacf458aded81d9d9e
Rafael <rafael@Rafaels-MacBook-Air.local>
2025-05-27 00:15:33 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+     import { defineConfig } from 'vite';
+     import laravel from 'laravel-vite-plugin';
+     import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-});
+     export default defineConfig({
+         root: 'resources/views', // Menentukan root folder
+         plugins: [
+             laravel({
+                 input: ['css/app.css', 'js/app.js'],
+                 refresh: true,
+             }),
+             tailwindcss(),
+         ],
+         build: {
+             outDir: '../dist', // Menentukan direktori output
+         },
+     });
+     
